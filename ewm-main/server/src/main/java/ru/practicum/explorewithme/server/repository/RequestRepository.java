b26b4f813ba8d40bfea578f2fcaed36495e75237
osk08880 <osk08880@gmail.com>
2026-01-03 01:53:05 +0300

    Fix 1

--- ewm-main/server/src/main/java/ru/practicum/explorewithme/server/repository/RequestRepository.java
+++ ewm-main/server/src/main/java/ru/practicum/explorewithme/server/repository/RequestRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.explorewithme.server.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.explorewithme.server.entity.Request;
 import ru.practicum.explorewithme.server.entity.RequestStatus;
-import ru.practicum.explorewithme.server.entity.User;
 
 import java.util.List;
 
