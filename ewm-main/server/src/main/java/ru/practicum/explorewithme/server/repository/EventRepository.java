5c5a3198bc8ec0f6f74d054b9c0ac27f36063b41
osk08880 <osk08880@gmail.com>
2026-01-11 21:58:53 +0300

    Fix 16

--- ewm-main/server/src/main/java/ru/practicum/explorewithme/server/repository/EventRepository.java
+++ ewm-main/server/src/main/java/ru/practicum/explorewithme/server/repository/EventRepository.java
@@ -16,20 +16,18 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     boolean existsByCategoryId(Long categoryId);
 
     @Query("""
-        SELECT e FROM Event e
-        WHERE (:text IS NULL
-               OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%'))
-               OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))
-               OR LOWER(e.title) LIKE LOWER(CONCAT('%', :text, '%')))
-          AND (:categories IS NULL OR e.category.id IN :categories)
-          AND e.state = :publishedState
-          AND (:paid IS NULL OR e.paid = :paid)
-          AND e.eventDate BETWEEN :rangeStart AND :rangeEnd
-          AND (:onlyAvailable IS NULL OR e.participantLimit = 0
-               OR e.participantLimit > (SELECT COUNT(r)
-                                        FROM Request r
-                                        WHERE r.event = e AND r.status = ru.practicum.explorewithme.server.entity.RequestStatus.CONFIRMED))
-    """)
+SELECT e FROM Event e
+WHERE (:text IS NULL
+       OR LOWER(CAST(e.annotation AS text)) LIKE LOWER(CONCAT('%', :text, '%'))
+       OR LOWER(CAST(e.description AS text)) LIKE LOWER(CONCAT('%', :text, '%'))
+       OR LOWER(CAST(e.title AS text)) LIKE LOWER(CONCAT('%', :text, '%')))
+  AND (:categories IS NULL OR e.category.id IN :categories)
+  AND e.state = :publishedState
+  AND (:paid IS NULL OR e.paid = :paid)
+  AND e.eventDate BETWEEN :rangeStart AND :rangeEnd
+  AND (:onlyAvailable IS NULL OR e.participantLimit = 0
+       OR e.participantLimit > (SELECT COUNT(r) FROM Request r WHERE r.event = e AND r.status = ru.practicum.explorewithme.server.entity.RequestStatus.CONFIRMED))
+""")
     List<Event> findPublicEvents(
             @Param("text") String text,
             @Param("categories") List<Long> categories,
@@ -38,8 +36,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             @Param("rangeEnd") LocalDateTime rangeEnd,
             @Param("onlyAvailable") Boolean onlyAvailable,
             @Param("publishedState") EventState publishedState,
-            Pageable pageable
-    );
+            Pageable pageable);
 
     @Query("""
         SELECT e FROM Event e
